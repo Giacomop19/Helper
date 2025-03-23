e8bd09170f26e9531e975bc5771fd44fe2cf7ded
Giacomo Pumapillo <80414903+Giacomop19@users.noreply.github.com>
2025-03-23 20:36:32 +0100

    Delete AppDelegate.m

--- AppDelegate.m
+++
@@ -1,6 +0,0 @@
-#import <GoogleMaps/GoogleMaps.h>
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [GMSServices provideAPIKey:@"AIzaSyBX-RlkMIiyLQ-WxYTeLJL7FreXpGD1N8k"];
-    return YES;
-}
